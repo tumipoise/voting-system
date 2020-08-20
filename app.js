25565174f177cc1b64a65e1086d7d4b689e3956e
Tumininu Sosanya <tumininu.sosanya@gmail.com>
2020-08-20 11:57:15 -0700

    Adding functionalities to the progress bar

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let voteNumber = document.getElementById("voteNumber");
+let progressBar = document.getElementById("myBar");
 let vn = 10;
 
 const nengiMinus = document.getElementById("nengiMinus");
@@ -11,7 +12,7 @@ const ozoPlus = document.getElementById("ozoPlus");
 
 const kiddwayaMinus = document.getElementById("kiddwayaMinus");
 const kiddwayaInput = document.getElementById("kiddwayaInput");
-const kiddwayaPlus = document.getElementById("kiddwayPlus");
+const kiddwayaPlus = document.getElementById("kiddwayaPlus");
 
 const ericaMinus = document.getElementById("ericaMinus");
 const ericaInput = document.getElementById("ericaInput");
@@ -26,40 +27,103 @@ const dorathyInput = document.getElementById("dorathyInput");
 const dorathyPlus = document.getElementById("dorathyPlus");
 
 nengiPlus.addEventListener("click", () => {
-    if (vn > 0) {
-      nengiInput.value++;
-      vn--;
-      voteNumber.textContent = vn;
-    }
-  });
+  if (vn > 0) {
+    nengiInput.value++;
+    vn--;
+    voteNumber.textContent = vn;
+    console.log(vn);
+    let voteLen = (vn * 100) / 10;
+    console.log(voteLen);
+    progressBar.style.width = `${voteLen}%`;
+  }
+});
 
 nengiMinus.addEventListener("click", () => {
   if (vn < 10) {
     nengiInput.value--;
     vn++;
     voteNumber.textContent = vn;
+    let voteLen = (vn * 100) / 10;
+    console.log(voteLen);
+    progressBar.style.width = `${voteLen}%`;
   }
 });
 
 ozoPlus.addEventListener("click", () => {
-    if (vn > 0) {
-        ozoInput.value++;
-        vn--;
-        voteNumber.textContent = vn;
-    }
+  if (vn > 0) {
+    ozoInput.value++;
+    vn--;
+    voteNumber.textContent = vn;
+  }
 });
 
 ozoMinus.addEventListener("click", () => {
-    if (vn < 10) {
-        ozoInput.value--;
-        vn++;
-        voteNumber.textContent = vn;
-    }
+  if (vn < 10) {
+    ozoInput.value--;
+    vn++;
+    voteNumber.textContent = vn;
+  }
+});
+
+kiddwayaPlus.addEventListener("click", () => {
+  if (vn > 0) {
+    kiddwayaInput.value++;
+    vn--;
+    voteNumber.textContent = vn;
+  }
 });
 
 kiddwayaMinus.addEventListener("click", () => {
-    
-})
+  if (vn < 10) {
+    kiddwayaInput.value--;
+    vn++;
+    voteNumber.textContent = vn;
+  }
+});
 
+ericaMinus.addEventListener("click", () => {
+  if (vn < 10) {
+    ericaInput.value--;
+    vn++;
+    voteNumber.textContent = vn;
+  }
+});
 
+ericaPlus.addEventListener("click", () => {
+  if (vn > 0) {
+    ericaInput.value++;
+    vn--;
+    voteNumber.textContent = vn;
+  }
+});
 
+layconMinus.addEventListener("click", () => {
+  if (vn < 10) {
+    layconInput.value--;
+    vn++;
+    voteNumber.textContent = vn;
+  }
+});
+layconPlus.addEventListener("click", () => {
+  if (vn > 0) {
+    layconInput.value++;
+    vn--;
+    voteNumber.textContent = vn;
+  }
+});
+
+dorathyMinus.addEventListener("click", () => {
+  if (vn < 10) {
+    dorathyInput.value--;
+    vn++;
+    voteNumber.textContent = vn;
+  }
+});
+
+dorathyPlus.addEventListener("click", () => {
+  if (vn > 0) {
+    dorathyInput.value++;
+    vn--;
+    voteNumber.textContent = vn;
+  }
+});
